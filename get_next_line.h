39515c87df17626a8831058cb5045060d355780c
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-02-26 19:35:50 +0100

    initial commit

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: fras <fras@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/02/26 18:46:02 by fras          #+#    #+#                 */
+/*   Updated: 2023/02/26 18:46:03 by fras          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
