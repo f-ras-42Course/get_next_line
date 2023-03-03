fc8feb632d0975bf4678c232e7b23d5c39e38f17
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-03-03 23:19:46 +0100

    make tester functional

--- gnlTesty.c
+++ gnlTesty.c
@@ -6,16 +6,21 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/28 21:57:30 by fras          #+#    #+#                 */
-/*   Updated: 2023/03/03 23:04:48 by fras          ########   odam.nl         */
+/*   Updated: 2023/03/03 23:19:28 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "stdio.h"
 
 int	main(void)
 {
 	int fd;
+	char *ptr;
 
 	fd = open(testing.txt);
-	get_next_line(fd);
+	ptr = get_next_line(fd);
+	printf("%s", ptr);
+	free(ptr);
+	return (0);
 }
