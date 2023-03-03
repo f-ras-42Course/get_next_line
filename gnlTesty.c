c9c5a0defdecdd5c279cb3bda71ea954bcf0a6e0
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-03-03 23:05:10 +0100

    tester update

--- gnlTesty.c
+++ gnlTesty.c
@@ -6,7 +6,7 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/28 21:57:30 by fras          #+#    #+#                 */
-/*   Updated: 2023/02/28 23:01:59 by fras          ########   odam.nl         */
+/*   Updated: 2023/03/03 23:04:48 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ int	main(void)
 	int fd;
 
 	fd = open(testing.txt);
-	getline(fd);
-	printf()
+	get_next_line(fd);
 }
