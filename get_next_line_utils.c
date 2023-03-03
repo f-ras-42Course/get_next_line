161c34696b217a45c8f6a6fc7ea44668e3d10b2b
Ferry Ras <57290903+f-ras@users.noreply.github.com>
2023-03-03 21:19:56 +0100

    reformatted file structure and function order

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,52 @@
 /*   By: fras <fras@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/26 18:45:41 by fras          #+#    #+#                 */
-/*   Updated: 2023/02/28 23:02:03 by fras          ########   odam.nl         */
+/*   Updated: 2023/03/03 21:19:36 by fras          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+size_t	newline_checker(char *search, size_t size)
+{
+	size_t	i;
+
+	while (i <= size)
+	{
+		if (search[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+void	to_string_alloc(char *src, char *dest, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	dest = malloc((size + 1) * sizeof(char));
+	while (i < size)
+			dest[i] = src[i++];
+	dest[i] = '\0';
+}
+
+void	to_string_realloc(char *src, char *dest, size_t prev_size, size_t size)
+{
+	char	*backup;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	backup = malloc(prev_size * sizeof(char));
+	while (i < prev_size)
+		backup[i] = dest[i++];
+	free (dest);
+	dest = malloc((size + 1) * sizeof(char));
+	while (i < prev_size)
+		dest[i] = backup[i++];
+	while (i < size)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+}
